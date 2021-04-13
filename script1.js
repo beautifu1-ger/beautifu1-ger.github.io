146e5e8d0077f773e721cab17b1f7cc3f6d8044e
beautifu1ger <80017913+beautifu1-ger@users.noreply.github.com>
2021-04-13 14:53:29 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,11 +1,9 @@
-
-
 function puzzle(){
   var response = prompt("小白+小白=?")
   var obj=document.getElementById('do')
       obj.innerHTML='再试一次';
  if (response=="小白兔")
-{  alert("太聪明了！不愧是你！");}
+{  alert("太聪明了!不愧是你!");}
  else {alert("想想谐音梗!————2怎么读？");}
 }
 
