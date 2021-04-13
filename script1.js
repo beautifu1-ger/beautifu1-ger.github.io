95f2bb41ecdfb8c3f3c0b9c84eaf17f1c8ed029b
beautifu1ger <80017913+beautifu1-ger@users.noreply.github.com>
2021-04-13 14:37:46 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+
+
+function puzzle(){
+  var response = prompt("小白+小白=?")
+  var obj=document.getElementById('do')
+      obj.innerHTML='再试一次';
+ if (response=="小白兔")
+{  alert("好耶！！");}
+ else {alert("想想谐音梗!————2怎么读？");}
+}
+
+function cold(){
+  var response = prompt("我的心,冷...");
+}
